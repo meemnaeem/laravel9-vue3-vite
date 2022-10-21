ef6fbe7f3ed3d3889b76f8bd9d4c597a2573bc07
Naeem <meemnaeem@gmail.com>
2022-10-21 00:33:24 -0400

    Vue component added and working

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,28 @@
-// vite.config.js
-import { defineConfig } from "vite";
-import laravel from "laravel-vite-plugin";
-import vue from "@vitejs/plugin-vue";
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
-    plugins: [vue(), laravel(["resources/css/app.css", "resources/js/app.js"])],
+    plugins: [
+        laravel({
+            input: [
+                'resources/sass/app.scss',
+                'resources/js/app.js',
+            ],
+            refresh: true,
+        }),
+        vue({
+            template: {
+                transformAssetUrls: {
+                    base: null,
+                    includeAbsolute: false,
+                },
+            },
+        }),
+    ],
+    resolve: {
+        alias: {
+            vue: 'vue/dist/vue.esm-bundler.js',
+        },
+    },
 });
